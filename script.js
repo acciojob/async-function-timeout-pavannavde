e80359ee53afc70c2602b970e13a434e1be49253
Pavan Navde <136722639+pavannavde@users.noreply.github.com>
2023-08-29 11:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,18 +2,18 @@
 
  let output =document.getElementById("output");
 let form = document.getElementById("btn");
-  const msg=document.getElementById("text").value;
-const delay=document.getElementById("delay").value;
+  let msg =document.getElementById("text").value;
+let delay =document.getElementById("delay").value;
 form.addEventListener("click",(e)=>{
 	e.preventDefault();
-	async function main(delay){
+	async function main(){
 
 	  await new Promise((resolve)=>{
 		  setTimeout(()=>{
 			  resolve();
-		  },delay)
-		 output.innerText=msg; 
+		  },delay);
+		 output.innerText="msg"; 
 	  })
-		}
+	}
 })
   
